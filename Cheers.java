96a0d90651ff9c39c0db8f2828bd742fa45a788c
elayhakim <elay.hakim@post.runi.ac.il>
2025-11-06 14:58:02 +0200

    1-4

--- Cheers.java
+++ Cheers.java
@@ -14,18 +14,23 @@ public class Cheers {
                 String firstlet = "" + name.charAt(0);
                 String anwords = "AaEeFfHhIiLlMmNnOoRrSsXx";
                 String little = "abcdefghijklmnopqrstuvwxyz";
+                String finish ="";
                 while (i < name.length()){
                         if(anwords.indexOf(name.charAt(i)) != -1){
                                 if(little.indexOf(name.charAt(i)) != -1){
                                         System.out.println("Give me an " + (char)(name.charAt(i)-32) + ": " + (char)(name.charAt(i)-32) + "!");
+                                        finish= "" + (char)(name.charAt(i)-32);
                                 }else{
                                         System.out.println("Give me an " + name.charAt(i) + ": " + name.charAt(i) + "!");
+                                        finish= "" + name.charAt(i);
                                 }
                         }else{
                                 if(little.indexOf(name.charAt(i)) != -1){
                                         System.out.println("Give me a  " + (char)(name.charAt(i)-32) + ": " + (char)(name.charAt(i)-32) + "!");
+                                        finish= "" + (char)(name.charAt(i)-32);
                                 }else{
                                         System.out.println("Give me a  " + name.charAt(i) + ": " + name.charAt(i) + "!");
+                                        finish= "" + name.charAt(i);
                                 }
                         }
                         i++;
