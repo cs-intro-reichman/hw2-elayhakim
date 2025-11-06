905a4829ce9d7a1c6a96dc0c6756bad873afc6cd
elayhakim <elay.hakim@post.runi.ac.il>
2025-11-06 14:01:05 +0200

    1-4

--- CalcPi.java
+++ CalcPi.java
@@ -14,7 +14,7 @@ public class CalcPi {
 			flag = !flag;
 			div+=2;
 		}
-		System.out.println("pi according to java: " + Math.PI);
-		System.out.println("pi, approximated: " + x*4);
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:    " + x*4);
 	}
 }
